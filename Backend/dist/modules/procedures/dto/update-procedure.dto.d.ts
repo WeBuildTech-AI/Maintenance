74ae77e83470415e28f45056c4917507d6a865d6
Prakhar <prakharsingh2018@gmail.com>
2025-09-23 16:31:40 +0530

    Stop tracking dist folder

--- Backend/dist/modules/procedures/dto/update-procedure.dto.d.ts
+++
@@ -1,5 +0,0 @@
-import { CreateProcedureDto } from './create-procedure.dto';
-declare const UpdateProcedureDto_base: import("@nestjs/mapped-types").MappedType<Partial<CreateProcedureDto>>;
-export declare class UpdateProcedureDto extends UpdateProcedureDto_base {
-}
-export {};
