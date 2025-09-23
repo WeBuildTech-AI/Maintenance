d37e2e4f1866d14e73329873e735c9e69ae4247f
ashwini-wbt <ashwini@webuildtech.in>
2025-09-23 12:19:09 +0530

    Added Swagger as well in the backend

--- Backend/dist/main.js
+++ Backend/dist/main.js
@@ -1,26 +1,51 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const common_1 = require("@nestjs/common");
-const config_1 = require("@nestjs/config");
-const core_1 = require("@nestjs/core");
-const app_module_1 = require("./app.module");
+import { ValidationPipe, VersioningType } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+
 async function bootstrap() {
-    const app = await core_1.NestFactory.create(app_module_1.AppModule, {
-        bufferLogs: true,
-    });
-    const configService = app.get(config_1.ConfigService);
-    app.setGlobalPrefix('api');
-    app.enableVersioning({
-        type: common_1.VersioningType.URI,
-        defaultVersion: '1',
-    });
-    app.useGlobalPipes(new common_1.ValidationPipe({
-        whitelist: true,
-        transform: true,
-        forbidNonWhitelisted: true,
-        transformOptions: { enableImplicitConversion: true },
-    }));
-    const port = configService.get('PORT', 3000);
-    await app.listen(port);
+  const app = await NestFactory.create(AppModule, {
+    bufferLogs: true,
+  });
+
+  const configService = app.get(ConfigService);
+
+  // Global prefix & versioning
+  app.setGlobalPrefix("api");
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: "1",
+  });
+
+  // Validation
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+      transformOptions: { enableImplicitConversion: true },
+    }),
+  );
+
+  // Swagger setup
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle("My API")
+    .setDescription("API documentation for my project")
+    .setVersion("1.0")
+    .addBearerAuth() // optional: adds JWT auth header in Swagger UI
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup("api/docs", app, document, {
+    swaggerOptions: { persistAuthorization: true },
+  });
+
+  const port = configService.get("PORT", 3000);
+  await app.listen(port);
+  console.log(`🚀 Application running on http://localhost:${port}/api`);
+  console.log(`📖 Swagger docs available at http://localhost:${port}/api/docs`);
 }
+
 bootstrap();
